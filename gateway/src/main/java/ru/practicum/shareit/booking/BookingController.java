63f4a5250a401e71200ecf38ea2b45c4ed615d14
m-goryunov <108279048+m-goryunov@users.noreply.github.com>
2023-07-06 10:57:26 +0300

    Review 2
    fixed checkstyle

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
--- gateway/src/main/java/ru/practicum/shareit/item/ItemClient.java
+++ gateway/src/main/java/ru/practicum/shareit/item/ItemClient.java
@@ -52,7 +52,7 @@ public class ItemClient extends BaseClient {
                 "from", from,
                 "size", size
         );
-        return get("/search?text={text}&from={from}&size={size}",null, parameters);
+        return get("/search?text={text}&from={from}&size={size}", null, parameters);
     }
 
     public ResponseEntity<Object> createComment(long itemId, CommentRequestDto commentDto, long userId) {
